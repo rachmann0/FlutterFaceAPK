cbd37a6eaa2374e2f321deb6a8b4530383af3c33
Luky <lukydwisaputra@Lukys-MacBook-Pro-3.local>
2023-07-15 20:16:22 +0700

    replace android module

--- android/app/src/main/java/com/example/sdk_test/MainActivity.java
+++ android/app/src/main/java/com/example/sdk_test/MainActivity.java
@@ -13,68 +13,26 @@ import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodChannel;
 
 public class MainActivity extends FlutterActivity {
-    FacePassHandler mFacePassHandler;
-    
-    private static final String CHANNEL = "channelname";
+  FacePassHandler mFacePassHandler;
 
-    @Override
-    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-        super.configureFlutterEngine(flutterEngine);
-        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL).setMethodCallHandler(
+  private static final String CHANNEL = "facepass";
+  private static final String DEBUG_TAG = "FACEPASS";
 
-/*
-                        var args = call.arguments as Map<String, String>;
-                        var message = args["message"];
-*/
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    super.configureFlutterEngine(flutterEngine);
+    new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL).setMethodCallHandler(
 
-                        (call, result) -> {
-                            //if ("logtest" == call.method) { // compare reference not value
-                            if ("logtest".equals(call.method)) {
-                                Toast.makeText(this, "logtest", Toast.LENGTH_LONG).show();
-                                Log.d(DEBUG_TAG, "onCreate");
-                                result.success("success");
-                            }
-                            // This method is invoked on the main thread.
-                            // TODO
-                        }
-                );
-    }
-    private static final String DEBUG_TAG = "DEBUGTAG";
-
-    //private void initFacePassSDK() throws IOException {
-/*        Log.d(DEBUG_TAG, "initFacePassSDK");
-
-        Context mContext = getApplicationContext();
-        FacePassHandler.initSDK(mContext);
-        if (authType == FacePassAuthType.FASSPASS_AUTH_MCVFACE) {
-            // face++授权
-            FacePassHandler.authPrepare(getApplicationContext());
-            FacePassHandler.getAuth(authIP, apiKey, apiSecret, true);
-        } else if (authType == FacePassAuthType.FACEPASS_AUTH_MCVSAFE) {
-            Log.d(DEBUG_TAG, "authType = FACEPASS_AUTH_MCVSAFE");
-            // 金雅拓授权接口
-            boolean auth_status = FacePassHandler.authCheck();
-            if ( !auth_status ) {
-                singleCertification(mContext);
-                auth_status = FacePassHandler.authCheck();
-            }
-
-            if ( !auth_status ) {
-                Log.d(DEBUG_TAG, "Authentication result : failed.");
-                Log.d("mcvsafe", "Authentication result : failed.");
-                // 授权不成功，根据业务需求处理
-                // ...
-                return;
-            }else {
-                Log.d("mcvsafe", "Authentication result : success.");
-            }
-        } else {
-            Log.d(DEBUG_TAG, "have no auth.");
-            Log.d("FacePassDemo", "have no auth.");
-            return ;
+      (call, result) -> {
+        //if ("logtest" == call.method) { // compare reference not value
+        if ("logtest".equals(call.method)) {
+          Toast.makeText(this, "logtest", Toast.LENGTH_LONG).show();
+          Log.d(DEBUG_TAG, "onCreate");
+          result.success("success");
         }
-
-        Log.d(DEBUG_TAG, "FacePassHandler.getVersion() = " + FacePassHandler.getVersion());*/
-//    }
-
+        // This method is invoked on the main thread.
+        // TODO
+      }
+    );
+  }
 }
